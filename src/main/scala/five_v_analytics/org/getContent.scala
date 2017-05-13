bf55fe8991a50a723c11b6282c3b9a7afc1a9277
davbzh <davit.bzhalava@ki.se>
2017-05-13 02:22:20 +0200

    getContent

---
+++ src/main/scala/five_v_analytics/org/getContent.scala
@@ -0,0 +1,35 @@
+package five_v_analytics.org
+
+/**
+  * Created by davbzh on 2017-05-13.
+  */
+
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.DefaultHttpClient
+import org.apache.http.util.EntityUtils
+
+object getContent {
+
+  def getNewRelicContentFromUrl(apikey: String, apiurl: String): Unit = {
+
+    //import sys.process._
+    //val data = List("curl", "-H", s"X-API-KEY: ${apikey}", s"${apiurl}").!!
+    //println("Shipping data is " + data)
+
+    val httpGet = new HttpGet(apiurl)
+    // set the desired header values
+    httpGet.setHeader("X-API-KEY", apikey)
+
+    // execute the request
+    val client = new DefaultHttpClient
+    val bla = client.execute(httpGet).getEntity
+    println(EntityUtils.toString(bla))
+    client.getConnectionManager.shutdown
+  }
+
+  def main(args: Array[String]) {
+
+    getNewRelicContentFromUrl(args(0), args(1))
+
+  }
+}
